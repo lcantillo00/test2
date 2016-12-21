866a28678b473cef2067b351f6785df179a85025
FVI <test@examle.com>
2016-12-21 18:08:57 -0500

    copy files from WEB1010

---
+++ Q5.js
@@ -0,0 +1,10 @@
+// Log to the console a Boolean expression that tests
+// whether two numbers are equal, and whose answer evaluates to false.
+var a=2;
+var b=4;
+if (a===b){
+  console.log("the numbers are equal");
+
+}else{
+  console.log("the numbers are different");
+}
